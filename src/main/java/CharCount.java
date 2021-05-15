45895d2f8e60bc79e282db7220a271c33cc1ed34
Abhishek85424 <abhi_siom_06@yahoo.co.in>
2021-05-15 20:27:50 +0530

    Initial commit

---
+++ src/main/java/CharCount.java
@@ -0,0 +1,27 @@
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class CharCount {
+    public static void main(String[] args) {
+        String s ="abbcdaae";
+        Set<Character> distinctEle1 = new HashSet<>();
+//        Using Stream
+        List<Character> strList = s.chars().mapToObj(x->(char)x).collect(Collectors.toList());
+        Set<Character> distinctEle = new HashSet<>(strList);
+
+//        Using for each loop
+        for(Character ch : s.toCharArray())
+        {
+            distinctEle1.add(ch);
+        }
+
+        for (Character ch : distinctEle)
+        {
+            int charCount = Collections.frequency(strList,ch);
+            System.out.println(String.format("Char : %c has count : %d",ch,charCount));
+        }
+    }
+}
