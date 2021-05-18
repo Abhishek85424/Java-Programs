2990b93562c2e67a7e727ba56cf5d1064842c531
Abhishek85424 <abhi_siom_06@yahoo.co.in>
2021-05-18 10:09:57 +0530

    Added code for Reverse StringReverseSpecialChar.

---
+++ src/main/java/StringReverseSpecialChar.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringReverseSpecialChar {
+    @Test
+    public void Testing()
+    {
+//        Reverse a string with special character without changing special char position
+        String str ="@bhis$ek";
+        HashMap<Integer,Character> hm = new HashMap<>();
+        StringBuffer reverseString= new StringBuffer(str.length());
+
+        for(int index=str.length()-1;index>=0;index--)
+        {
+            if(!Character.isLetter(str.charAt(index)))
+           for(int key : hm.keySet())
+           {
+               reverseString.insert(key,hm.get(key));
+           }
+        }
+        System.out.println(String.format("Reveresed String %s",reverseString));
+    }
+
+}
