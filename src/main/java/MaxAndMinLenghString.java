2aa308835ecc940bdc0b092198c0a69e29a5b93f
Abhishek85424 <abhi_siom_06@yahoo.co.in>
2021-05-18 22:11:17 +0530

    Added code for MaxAndMinLenghString.

---
+++ src/main/java/MaxAndMinLenghString.java
@@ -0,0 +1,24 @@
+public class MaxAndMinLenghString {
+    public static void main(String[] args) {
+        String str = "Hello this is successful string";
+        String max="",min="";
+        boolean first = true;
+        for(String word : str.split(" "))
+        {
+            if(first)
+            {
+                first = false;
+                min = word;
+                max = word;
+            }
+            else
+            {
+                if(word.length() > max.length())
+                    max = word;
+                if(word.length() < min.length())
+                    min = word;
+            }
+        }
+        System.out.println(String.format("Max length string : %s and min length string : %s",max,min));
+    }
+}
