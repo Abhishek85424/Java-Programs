27b6af57322f50871eb2721cf9d76db6d090af47
Abhishek85424 <abhi_siom_06@yahoo.co.in>
2021-05-23 14:36:32 +0530

    Total 9 Count

---
+++ src/main/java/com/number/Number9Count.java
@@ -0,0 +1,21 @@
+package com.number;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Number9Count {
+    public static void main(String[] args) {
+//        Print total occurence of 9 in range of 9-999
+        int total9Count =0;
+        for(int index = 9; index<=999;index++)
+        {
+            if(String.valueOf(index).contains("9"))
+            {
+                System.out.println(index);
+                total9Count += Collections.frequency(Arrays.asList(String.valueOf(index).split("")),"9");
+            }
+        }
+        System.out.println(total9Count);
+
+    }
+}
